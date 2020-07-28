7880ea07fb9cd987cd35ed6de35f449d24ab8c67
Zane Deupree <66035453+Zane-Deupree@users.noreply.github.com>
2020-07-28 10:52:56 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
-//Declare the installed framesworks
-const express = require('express');
-const bodyParser = require('body-parser');
-//call the express and Body-parser
-let app = express();
-app.use(bodyParser.urlencoded({
-extended: true
-}));
-//we installed the ejs and created a file inside the views
-app.set('view engine', 'ejs');
+// Form reference
+const form = {}
+form.noteText = document.querySelector('#formNoteText');
+form.addButton = document.querySelector('#formAddButton');
+form.color = document.querySelector('#formColor');
 
-app.listen(101, function()
-{console.log("NoteApp server is running at port 101...")
-});
+const notes = document.querySelector('#notes');
+
+form.noteText.focus();
+
+// Functions
+function addNote() {
+  let text = form.noteText.value;
+  let note = document.createElement('div');
+  let deleteButton = document.createElement('span');
+
+  note.classList.add('note');
+  note.classList.add(form.color.value);
+  note.innerHTML = `<div class='note-text'>${text}</div>`;
+  deleteButton.classList.add('note-delete');
+  deleteButton.innerHTML = '&times;';
+
+  note.appendChild(deleteButton);  
+  notes.appendChild(note);
+
+  form.noteText.value = '';
+  form.noteText.focus();
+
+  addListenerDeleteButton(deleteButton);
+}
+
+function addListenerDeleteButton(deleteButton) {
+  deleteButton.addEventListener('click', function (e) {
+    e.stopPropagation();      
+    deleteNote(e);
+  });
+}
+
+function deleteNote(e) {
+  let eventNote = e.target.parentNode;
+  eventNote.parentNode.removeChild(eventNote);
+}
+
+
+
+// Event Listeners
+form.addButton.addEventListener('click', function (e) {
+  e.preventDefault();  
+  if (form.noteText.value != '') {
+    addNote();
+  }
+})
